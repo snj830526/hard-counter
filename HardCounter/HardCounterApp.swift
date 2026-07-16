87cf23beb6e6fd6f8b110d49e2f1bfecddb86c6f
soonispapa <snj830526@gmail.com>
2026-07-17 07:35:49 +0900

    Initial Commit

---
+++ HardCounter/HardCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardCounterApp.swift
+//  HardCounter
+//
+//  Created by john choi on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct HardCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
