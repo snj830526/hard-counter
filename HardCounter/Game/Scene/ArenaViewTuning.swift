6384c210a7181dc59e8ee8de087adae1e6b914f2
soonispapa <snj830526@gmail.com>
2026-07-18 15:30:49 +0900

    fix: reduce arena presentation zoom

--- HardCounter/Game/Scene/ArenaViewTuning.swift
+++ HardCounter/Game/Scene/ArenaViewTuning.swift
@@ -8,14 +8,14 @@ enum ArenaViewTuning {
     static let startingHorizontalOffset: CGFloat = 185
     static let startingDepthOffset: CGFloat = 36
 
-    static let baseZoom: CGFloat = 2.24
-    static let farZoom: CGFloat = 2.05
-    static let closeZoom: CGFloat = 2.55
+    static let baseZoom: CGFloat = 2.00
+    static let farZoom: CGFloat = 1.72
+    static let closeZoom: CGFloat = 2.12
     static let closeSeparation: CGFloat = 46
     static let farSeparation: CGFloat = 275
     static let zoomResponse: CGFloat = 3.8
 
-    static let fighterScaleBoost: CGFloat = 1.12
+    static let fighterScaleBoost: CGFloat = 1.25
     static let playerFocusWeight: CGFloat = 0.58
     static let cameraFollowResponse: CGFloat = 6.4
     static let horizontalFitFraction: CGFloat = 0.76
